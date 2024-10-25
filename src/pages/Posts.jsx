6edfc90ea7de11ce2fc7c4d57d39866bad17cadf
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-10-25 02:00:24 +0100

    axios practise react

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
+
 const Posts = () => {
-    const params = useParams();
-    console.log(params);
+    const { id } = useParams();
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+           const { data } =  await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+            setPosts(data);
+        }
+        fetchPosts();
+    }, []);
+
     return (
         <div>
-            Posts
+            {posts.map(post => <div>{post.id}</div>)}
         </div>
-    )
+    );
 }
 
 export default Posts;
